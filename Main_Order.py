2c6bcd94b791b61c1b42ff2d52998d57f09a0302
Utai <74049768+UtaiC@users.noreply.github.com>
2024-10-10 10:27:46 +0700

    Sep-Update

--- Main_Order.py
+++ Main_Order.py
@@ -91,6 +91,7 @@ elif page == "Stock Update":
 
 # Sales Page #################################################
 elif page == "Sales Order":
+    st.subheader("Update Sep-10-2024")
     st.subheader("ตรวจสอบรายการขาย")
     ############################
     ############################
