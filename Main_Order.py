a4c3d2a6c5fb0b9e6349438dbe8d45f4772b914f
Utai <74049768+UtaiC@users.noreply.github.com>
2024-10-26 14:24:23 +0700

    update V

--- Main_Order.py
+++ Main_Order.py
@@ -286,8 +286,9 @@ elif page == "Print Order":
         
     #########################################################
     # #######################
-    Area=r"This PC/Users/utaic/LEX-WAREHOUSE/"
-    Sales=pd.read_excel(v+Sales_No+'Sales-Update.xlsx')
+    Area=r"C:\Users\utaie\LEX-WAREHOUSE"
+    Sales=pd.read_excel(Area+Sales_No+'Sales-Update.xlsx')
+    Sales
     ############
     Sales=Sales[Sales["Timestamp"].str.contains(Sales_Date)]
     Sales=Sales[Sales["เลขที่ขายสินค้า"].str.contains(Sales_No)]
